cec3890e9fb8d7643e7461186a9868bbcfc5a3d6
Qin Ying Chen <qinyingchen@qycs-MBP.fios-router.home>
2022-11-29 01:41:38 -0500

    added pages with manifesto and existing works

--- puzzle.js
+++ puzzle.js
@@ -70,6 +70,26 @@ window.onload = function(){
         // add piece to pieces holder
         document.getElementById('pieces').append(piece);
     }
+
+    // hamburger menu w/ overlay logic
+    const sideNav = document.querySelector(".sideNav");
+    const overlay = document.querySelector(".overlay");
+    const ham = document.querySelector(".ham");
+    const menuX = document.querySelector(".menuX");
+    const menuItems = document.querySelectorAll(".menuLink");
+
+    menuItems.forEach(menuItem => {
+        menuItem.addEventListener("click", toggleHamburger);
+    });
+
+    ham.addEventListener("click", toggleHamburger);
+    menuX.addEventListener("click", toggleHamburger);
+    overlay.addEventListener("click", toggleHamburger);
+
+    function toggleHamburger() {
+        overlay.classList.toggle("showOverlay");
+        sideNav.classList.toggle("showNav");
+    }
 }
 
 function dragStart() {
